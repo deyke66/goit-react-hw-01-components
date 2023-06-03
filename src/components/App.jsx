c43f75a2ef414aa6f0614aece359b3b6af04d4c3
Kurnosov Valentyn <deyke66@gmail.com>
2023-06-03 11:55:05 +0200

    first-task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import user from "../user.json";
+import { Profile } from './profile/profile';
+
+
 export const App = () => {
   return (
     <div
@@ -7,10 +11,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile {...user} />
     </div>
   );
 };
---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+// import styles from './profile.module.css';
+
+export const Profile = ({ username, tag, location, avatar, stats: {followers, views, likes}}) => (
+    <div className="profile">
+        <div className="description">
+          <img
+            src={avatar}
+            alt="User avatar"
+            className="avatar"
+          />
+          <p className="name">{username}</p>
+          <p className="tag">{tag}</p>
+          <p className="location">{location}</p>
+        </div>
+
+        <ul className="stats">
+          <li>
+            <span className="label">Followers</span>
+            <span className="quantity">{followers}</span>
+          </li>
+          <li>
+            <span className="label">Views</span>
+            <span className="quantity">{views}</span>
+          </li>
+          <li>
+            <span className="label">Likes</span>
+            <span className="quantity">{likes}</span>
+          </li>
+        </ul>
+      </div>
+)
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.object,
+
+}
