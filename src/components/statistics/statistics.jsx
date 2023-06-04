5e168f0cba87a83a5eb05eda5e19041fcb27a1a0
Kurnosov Valentyn <deyke66@gmail.com>
2023-06-04 17:20:20 +0200

    task-2
    finished styles for statistic section

--- src/components/statistics/statistics.jsx
+++ src/components/statistics/statistics.jsx
@@ -1,17 +1,25 @@
+import styles from './statistics.module.css';
+
 export const Statistics = props => {
   const { title, stats } = props;
   return (
     <section className="statistics">
-      {title && <h2 className="title">Upload stats</h2>}
+      {title && <h2 className={styles.title}>Upload stats</h2>}
 
-      <ul className="stat-list">
+      <ul className={styles.stat_list}>
         {stats.map(item => (
-          <li key={item.id} className="item">
-            <span className="label">{item.label}</span>
-            <span className="percentage">{item.percentage}</span>
+          <li key={item.id} className={styles.item} style={{
+            backgroundColor: randomBackgroundColor()
+          }}>
+            <span className={styles.label}>{item.label}</span>
+            <span className="percentage">{item.percentage}%</span>
           </li>
         ))}
       </ul>
     </section>
   );
 };
+
+function randomBackgroundColor() {
+  return `#${Math.floor(Math.random()*16777215).toString(16)}`
+}
